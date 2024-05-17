a2b8ddb7489090b1c5a4200376ee9b62c3f69125
danyGhassan <dany.ghassa@gmail.com>
2024-05-17 14:54:09 +0200

    test

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'setAlarm') {
-      const alarmTime = message.time;
-  
-      chrome.alarms.create('alarm', { when: alarmTime });
-  
-      chrome.alarms.onAlarm.addListener((alarm) => {
-        if (alarm.name === 'alarm') {
-          chrome.notifications.create('alarm-notification', {
-            type: 'basic',
-            iconUrl: 'icon48.png',
-            title: 'Alarm',
-            message: 'It\'s time!'
-          });
-        }
-      });
-    }
-  });
-  
+  if (message.action === 'setAlarm') {
+    const alarmTime = message.time;
+
+    chrome.alarms.create('alarm', { when: alarmTime });
+
+    chrome.alarms.onAlarm.addListener((alarm) => {
+      if (alarm.name === 'alarm') {
+        chrome.notifications.create('alarm-notification', {
+          type: 'basic',
+          iconUrl: 'icon48.png',
+          title: 'Alarm',
+          message: 'It\'s time!',
+          requireInteraction: true
+        }, () => {
+          // Play the alarm sound when the notification is created
+          const audio = new Audio(chrome.runtime.getURL('alarm.mp3'));
+          audio.play();
+        });
+      }
+    });
+  }
+});
